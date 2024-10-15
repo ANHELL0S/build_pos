1e2da37b5da4c158c0b5a098cae3981ede5bd968
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-10-15 01:55:07 -0500

    Delete assets directory

--- assets/kardex.api-BjsjBSbb.js
+++
@@ -1,1 +0,0 @@
-import{aX as s}from"./index-D8Kk0eQ9.js";const a=async()=>{try{return(await s.get("/")).data}catch(e){throw new Error(`${e.response.data.message}`)}},o=async e=>{try{return(await s.get(`/${e}`)).data}catch(r){throw new Error(`${r.response.data.message}`)}};export{a,o as g};
